5e459a867edbaf85d847c29166e9bd9e1118079f
Harel <harelperi2@gmail.com>
2020-04-09 17:56:55 -0400

    vars

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,8 +1,15 @@
 <?php 
 
  // db settings
-	$host = 'localhost';
-	$user = 'harel';
-	$pass = 'asdasd';
-	$database = 'lists';
+	// $host = 'sql9.freesqldatabase.com'; //sql9.freesqldatabase.com
+	// $user = 'sql9332428'; // sql9332428
+	// $pass = 'vVK7bJ7W6w'; // vVK7bJ7W6w
+	// $database = 'sql9332428'; // 	sql9332428
+
+	$_ENV["USER"]
+
+	$host = $_ENV["host"]; //sql9.freesqldatabase.com
+	$user = $_ENV["user"]; // sql9332428
+	$pass = $_ENV["pass"]; // vVK7bJ7W6w
+	$database = $_ENV["database"]; // 	sql9332428
 ?>
