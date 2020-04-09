0e0aebf6f6a1701df66b0e3afb0cdac919d873a1
Harel Peri <41084847+harelp@users.noreply.github.com>
2020-04-09 18:32:17 -0400

    Update db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,15 +1,4 @@
 <?php 
 
- // db settings
-	// $host = 'sql9.freesqldatabase.com'; //sql9.freesqldatabase.com
-	// $user = 'sql9332428'; // sql9332428
-	// $pass = 'vVK7bJ7W6w'; // vVK7bJ7W6w
-	// $database = 'sql9332428'; // 	sql9332428
-
-	$_ENV["USER"]
-
-	$host = $_ENV["host"]; //sql9.freesqldatabase.com
-	$user = $_ENV["user"]; // sql9332428
-	$pass = $_ENV["pass"]; // vVK7bJ7W6w
-	$database = $_ENV["database"]; // 	sql9332428
+ // db settings hidden 
 ?>
